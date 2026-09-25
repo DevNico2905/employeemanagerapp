f9b965e1afb500ed6928eee646f701607a17352a
DevNico2905 <nbernal36@unisalle.edu.co>
2026-09-25 12:53:04 -0500

    getEmployees on the component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   imports: [RouterOutlet],
@@ -8,5 +11,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
+
+  public employees: Employee[] = [];
+
+  private employeeService = inject(EmployeeService);
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
   protected readonly title = signal('employeemanagerapp');
 }
